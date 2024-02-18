d986195e50eb503a63dd11a2d84362e8f26f3292
alex_shklyar <136641877+Alex11SW@users.noreply.github.com>
2024-02-18 17:18:20 +0200

    Updates

---
+++ static/js/554.4ac2c9f4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[554],{554:(e,s,a)=>{a.r(s),a.d(s,{default:()=>c});var t=a(87);const n={};var o=a(184);const c=()=>(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{className:n.NotFoundpage,children:"Cannot found this page"}),(0,o.jsx)(t.rU,{to:"/",children:"Tomain page"})]})}}]);
+//# sourceMappingURL=554.4ac2c9f4.chunk.js.map
